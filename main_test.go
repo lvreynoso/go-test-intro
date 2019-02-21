8d883ba2c7d15cb8fccc77138253bc7c726786ce
Lucia Reynoso <lvreynoso@protonmail.com>
2019-02-21 14:12:15 -0800

    part 2

--- main_test.go
+++ main_test.go
@@ -28,3 +28,19 @@ func TestTableCalculate(t *testing.T) {
         }
     }
 }
+
+// func BenchmarkCalculate(b *testing.B) {
+//     for i := 0; i < b.N; i++ {
+//         Calculate(2)
+//     }
+// }
+
+func benchmarkCalculate(input int, b *testing.B) {
+    for n := 0; n < b.N; n++ {
+        Calculate(input)
+    }
+}
+
+func BenchmarkCalculate100(b *testing.B)         { benchmarkCalculate(100, b) }
+func BenchmarkCalculateNegative100(b *testing.B) { benchmarkCalculate(-100, b) }
+func BenchmarkCalculateNegative1(b *testing.B)   { benchmarkCalculate(-1, b) }
